64e96805d797c8fe0821174f0a32f9f514ef523d
jianwei-x <xjv1195275315@gmail.com>
2017-10-08 14:46:35 +0800

    add log controll

--- utils.py
+++ utils.py
@@ -2,26 +2,42 @@ import time
 
 
 class Log(object):
+    """
+    design like this for external easy to use,
+    you can just use like log.d log.w to print any lever you want ,
+    by way of set self.filter to different value, you can easy to control all your print
+    """
+
     def __print(self, lever, text, **kwargs):
-        print("[{}]  {}\t{}".format(lever, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) , text))
+        if lever >= self.filter:
+            print("[{}]  {}\t{}".format(lever, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), text), kwargs)
 
     def f(self, text, **kwargs):
-        self.__print("F", text, **kwargs)
+        self.__print(self.FATAL, text, **kwargs)
 
     def e(self, text, **kwargs):
-        self.__print("E", text, **kwargs)
+        self.__print(self.ERROR, text, **kwargs)
 
     def w(self, text, **kwargs):
-        self.__print("W", text, **kwargs)
+        self.__print(self.WARNING, text, **kwargs)
 
     def d(self, text, **kwargs):
-        self.__print("D", text, **kwargs)
+        self.__print(self.DEBUG, text, **kwargs)
 
     def i(self, text, **kwargs):
-        self.__print("I", text, **kwargs)
+        self.__print(self.INFO, text, **kwargs)
 
     def v(self, text, **kwargs):
-        self.__print("V", text, **kwargs)
+        self.__print(self.VERBOSE, text, **kwargs)
+
+    def __init__(self):
+        self.FATAL = 5
+        self.ERROR = 4
+        self.WARNING = 3
+        self.DEBUG = 2
+        self.INFO = 1
+        self.VERBOSE = 0
+        self.filter = self.VERBOSE
 
 
 log = Log()
