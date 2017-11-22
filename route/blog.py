ee5b9f98999258f585a8f6042753297781d16b78
jianwei-x <xjv1195275315@gmail.com>
2017-11-22 23:04:50 +0800

    add about page and journal page

--- route/blog.py
+++ route/blog.py
@@ -3,22 +3,26 @@ from route import http_response as response
 
 
 def index(request):
-    body = j.template("blog_index.html")
+    body = j.template("blog/index.html")
     return response(body)
 
 
-def list(request):
-    pass
+def journal(request):
+    body = j.template("blog/journal.html")
+    log.d("journal", "body is \n{}".format(body))
+    return response(body)
 
 
-def detail(request):
-    pass
+def about(request):
+    body = j.template("blog/about.html")
+    log.d("about", "body is \n{}".format(body))
+    return response(body)
 
 
 def route_dict():
     r = {
         "/": index,
-        "/list": list,
-        "/detail": detail,
+        "/journal": journal,
+        "/about": about,
     }
     return r
