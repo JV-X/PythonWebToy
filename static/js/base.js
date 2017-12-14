c5597a1606f761128e9242aa1306a75716e331c5
jianwei-x <xjv1195275315@gmail.com>
2017-12-14 23:02:48 +0800

    add base js lib

---
+++ static/js/base.js
@@ -0,0 +1,27 @@
+var log = function() {
+    console.log.bind(console, arguments)
+}
+
+var e = function(sel) {
+    return document.querySelector(sel)
+}
+
+var timeString = function(timestamp) {
+    var d = new Date(timestamp*1000)
+    return d.toLocaleString()
+}
+
+var ajax = function(method, path, data, responseCallback) {
+    var r = new XMLHttpRequest()
+
+    r.open(method, path, true)
+    r.setRequestHeader('Content-Type', 'application/json')
+    r.onreadystatechange = function() {
+        if(r.readyState === 4) {
+            responseCallback(r.response)
+        }
+    }
+    data = JSON.stringify(data)
+
+    r.send(data)
+}
