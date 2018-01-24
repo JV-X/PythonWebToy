efb009839c42a93fda72176486f7a4c09ae57902
jianwei-x <xjv1195275315@gmail.com>
2018-01-24 23:31:06 +0800

    little improve

--- route/routes_blog.py
+++ route/routes_blog.py
@@ -7,28 +7,25 @@ TAG = "routes_blog"
 
 
 def index(request):
-    body = jinja.template("blog/index.html", homepage=config.config['homepage'], owner=config.config["owner"])
+    body = jinja.template("blog/index.html", config=config.config)
     return response(body)
 
 
 def journal(request):
     id = request.query['id']
     j = Journal.find_by_id(id)
-    body = jinja.template("blog/journal.html", journal=j, owner=config.config["owner"])
+    body = jinja.template("blog/journal.html", journal=j, config=config.config)
     return response(body)
 
 
 def journals(request):
     a = Journal.all()
-    body = jinja.template("blog/journals.html", journals=a, homepage=config.config['homepage'],
-                          owner=config.config["owner"])
-    log.d("journal", "body is \n{}".format(body))
+    body = jinja.template("blog/journals.html", journals=a, config=config.config)
     return response(body)
 
 
 def about(request):
-    body = jinja.template("blog/about.html", homepage=config.config['homepage'], owner=config.config["owner"])
-    log.d("about", "body is \n{}".format(body))
+    body = jinja.template("blog/about.html", config=config.config)
     return response(body)
 
 
